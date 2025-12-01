f48dce6528a77073f536b35c85a832f6e7ea4426
Ayana Griffin <ayanajg@gmail.com>
2025-11-30 20:19:06 -0800

    chat keyboard pushing

--- Theo/app/chat.tsx
+++ Theo/app/chat.tsx
@@ -381,7 +381,7 @@ export default function ChatScreen() {
       <KeyboardAvoidingView
         style={styles.container}
         behavior={Platform.OS === "ios" ? "padding" : "height"}
-        keyboardVerticalOffset={Platform.OS === "ios" ? 60 : 0}
+        //keyboardVerticalOffset={Platform.OS === "ios" ? 60 : 0}
       >
         <View style={styles.headerRow}>
           <Pressable
